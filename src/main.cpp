f59890f3556cc0425ef5dd30ea00bd06800dcacc
jkhdr <redhydratechnologies@gmail.com>
2023-06-07 10:12:16 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ void setup(){
     back.write(94); //makes the back wheels spin at a relatively slow rate
     Serial.begin(9600);
     back.attach(9); //48,9
-    steer.attach(8); //13
+    steer.attach(8); //36:13
     steer.write(steer.read()+10); //makes a steer at 10 degrees to the right
 }
 void loop(){
